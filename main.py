4ef22f29e69763d4c6ee5433c61382cc2698c9dc
Снетков Роман <snetkov.roman@gmail.com>
2019-09-11 22:38:47 +0700

    Работает двумерный случай

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from collections import namedtuple
 
 
 def noise(sigma, N):
-    return [np.random.normal(0, np.sqrt(sigma)) for i in range(N)]
+    mean = np.zeros(sigma.shape[0])
+    return [np.random.multivariate_normal(mean, np.sqrt(sigma)) for i in range(N)]
+
 
 def one_dim():
     TimeStep = namedtuple('TimeStep', ['start', 'end'])
@@ -15,7 +17,7 @@ def one_dim():
     u = copy.copy(t)
     estimator = MaxLikelihood(Q=0.1, R=0.3, P=0.1,
                               x_0=0, F=tetha[0], H=1, Gamma=1,
-                              t=t, nu=1, u=u, N=10, a=tetha[1])
+                              t=t, nu=1, u=u, N=10, phi=1)
 
     x_array = []
     x1 = np.random.normal(estimator.x0, np.sqrt(estimator.p0))
@@ -31,3 +33,40 @@ def one_dim():
 
     new_array = minimize(estimator.estimate, [-2, 2], method='SLSQP', bounds=[[-3, -0.5], [0.5, 3]])
     print(new_array)
+
+
+def two_dim():
+    TimeStep = namedtuple('TimeStep', ['start', 'end'])
+    tetha = [1, -1]
+    t = list(range(11))
+    u = [1] * 10
+    estimator = MaxLikelihood(Q=0.1*np.eye(2),
+                              R=0.3*np.eye(2),
+                              P=np.array([[0.05, 0], [0, 0.05]]),
+                              x_0=np.array([-5, 0]),
+                              F=np.array([[0, 1], [-tetha[0], tetha[1]]]),
+                              H=np.eye(2),
+                              Gamma=np.eye(2),
+                              t=t,
+                              nu=1,
+                              u=u,
+                              N=10,
+                              phi=np.array([[0], [tetha[1]]]))
+
+    x_array = []
+    x1 = np.random.multivariate_normal(estimator.x0, np.sqrt(estimator.p0))
+    for i in range(len(t) - 1):
+        step = TimeStep(t[i], t[i + 1])
+        x_array.append(estimator.eq_for_x(step, x1, u[i], tetha))
+        x1 = x_array[i]
+    print(x_array)
+    x_noisy_array = x_array + noise(estimator.Q,  len(x_array))
+
+    y_array = [estimator.H @ x for x in x_noisy_array]
+    y_array += noise(estimator.R, len(x_array))
+    estimator.y = y_array
+
+    new_array = minimize(estimator.estimate, [2, -2], method='SLSQP', bounds=[[0.5, 3], [-3, -0.5]])
+    print(new_array)
+
+two_dim()
--- max_likelihood.py
+++ max_likelihood.py
@@ -6,8 +6,8 @@ from collections import namedtuple
 
 class MaxLikelihood:
     def __init__(self, Q, R, H,
-                 x_0, P, t, a,
-                 F, Gamma, nu, u, N):
+                 x_0, P, t,
+                 F, Gamma, nu, u, N, phi):
         self._Q = Q
         self._R = R
         self.x_0 = x_0
@@ -19,7 +19,7 @@ class MaxLikelihood:
         self.nu = nu
         self.Gamma = Gamma
         self.u = u
-        self.a = a
+        self.phi = phi
 
     @property
     def x0(self):
@@ -51,23 +51,27 @@ class MaxLikelihood:
             self._y = copy.copy(_y)
 
     def calc_P(self, P, t):
-        return self.F * P + P * self.F + self.Gamma * self.Q * self.Gamma
+        p = np.diagflat(P)
+        return np.diag(self.F @ p + p @ self.F.T + self.Gamma @ self.Q @ self.Gamma)
 
     def eq_for_p(self, time_step, x0):
         t = np.linspace(time_step.start, time_step.end, 10)
-        x = odeint(self.calc_P, x0, t)
-        x = np.array(x).flatten()
-        return x[len(x) - 1]
+        x = odeint(self.calc_P, np.diag(x0), t)
+        return np.diagflat([x[:, i][-1] for i in range(len(x0))])
 
     def _dxdt(self, x, t, tetha_1, tetha_2, u):
-        return tetha_1 * x[0] + u * tetha_2
+        return tetha_1 * x + self.phi * u * tetha_2
+
+    def _dxdt_2(self, x, t, tetha_1, tetha_2, u):
+        dxdt_1 = x[1]
+        dxdt_2 = -tetha_1 * x[0] + tetha_2 * x[1] + u * tetha_1
+        return [dxdt_1, dxdt_2]
 
     def eq_for_x(self, time_step, x, u, estimated_values):
         t_tk = np.linspace(time_step.start, time_step.end, 10)
         x0 = x
-        x = odeint(self._dxdt, x0, t_tk, args=(estimated_values[0], estimated_values[1], u))
-        x = np.array(x).flatten()
-        return x[len(x) - 1]
+        x = odeint(self._dxdt_2, x0, t_tk, args=(estimated_values[0], estimated_values[1], u))
+        return [x[:, i][-1] for i in range(len(x0))]
 
     def estimate(self, params):
         x_tk = self.x_0
@@ -78,12 +82,12 @@ class MaxLikelihood:
             step = TimeStep(self.t[i], self.t[i + 1])
             x = self.eq_for_x(step, x_tk, self.u[i], params)
             p = self.eq_for_p(step, p_tk)
-            e_tk = self._y[i] - self.H * x
-            B = self.H * p * self.H + self.R
-            K = p * self.H / B
-            x_tk = x + K * e_tk
-            p_tk = (1 - K - self.H) * p
-            result += e_tk * (1 / B) * e_tk + 0.5 * self.nu * B
+            e_tk = self._y[i].T - self.H @ x
+            B = self.H @ p @ self.H + self.R
+            K = p @ self.H @ np.linalg.inv(B)
+            x_tk = x + K @ e_tk
+            p_tk = (np.eye(self.F.shape[0]) - K @ self.H) * p
+            result += e_tk @ np.linalg.inv(B) @ e_tk.T + 0.5 * self.nu * np.log(np.linalg.det(B))
         result /= 2
         return result
 
